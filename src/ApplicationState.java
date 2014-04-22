224656335e93d8b127246f034a764dfad3453f35
ginasalh <ginasalh@users.noreply.github.com>
2014-04-22 13:46:51 -0700

    Update ApplicationState.java

--- src/ApplicationState.java
+++ src/ApplicationState.java
@@ -1,2 +1,45 @@
-/*
-*/
+import java.util.ArrayList;
+/**
+ * This class keeps track of the state of the game and the board.
+ * It keeps track of which views are being displayed. 
+ * 
+ * @author
+ */
+public class ApplicationState
+{
+    private ArrayList<View> views;
+    private Board board;
+    private int state;
+    private Board currentBoard;
+    private View displyedView;
+   // private View[] theviews;
+    
+    public ApplicationState()
+    {
+        views = new ArrayList<>();
+        board = new Board();
+        
+    } 
+    /**
+     * 
+     */
+    public void stateChanged()
+    {
+        //redraw everything
+    }
+    /**
+     * Updates the state of the application.
+     */
+    public void updateState()
+    {
+        //
+    }
+    
+    /**
+     * Updates 
+     */
+    public void updateGameMode()
+    {
+        
+    }
+}
