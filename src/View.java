b212187884eef6cc496e1a97ebb9ab242cad7c7b
ginasalh <ginasalh@users.noreply.github.com>
2014-04-22 13:50:10 -0700

    Update View.java

--- src/View.java
+++ src/View.java
@@ -1,2 +1,9 @@
-/*
-*/
+/**
+ * This is an interface which will be implemented by the other view classes. 
+ * 
+ * @author P.U.G.S.
+ */
+public interface View
+{
+    public void paintComponent();
+}
