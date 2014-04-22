f0c8fae7983f9b61e9c51f03a1d01120f3637e68
ginasalh <ginasalh@users.noreply.github.com>
2014-04-22 13:49:32 -0700

    Update NumberGameView.java

--- src/NumberGameView.java
+++ src/NumberGameView.java
@@ -1,2 +1,12 @@
+
 /*
-*/
+ * This class gives us version of the board game,
+ * in which the numbers are displayed in the pits,
+ * representing the number of stones.
+ *
+ * @author P.U.G.S.
+ */
+public class NumberGameView
+{
+    
+}
