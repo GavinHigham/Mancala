70e4bc3e305baff8c7959895184cc99e7e9afee2
Gavin Higham <Gavin.Higham@gmail.com>
2014-04-22 16:38:23 -0700

    JComponent to JPanel

--- src/View.java
+++ src/View.java
@@ -1,4 +1,4 @@
-import javax.swing.JComponent;
+import javax.swing.JPanel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -7,7 +7,7 @@ import javax.swing.event.ChangeListener;
  * 
  * @author P.U.G.S.
  */
-abstract class View extends JComponent implements ChangeListener
+abstract class View extends JPanel implements ChangeListener
 {
 	int[] board; //For display purposes. Local copy of boards state, may be stale if not updated.
 	boolean player1Turn; //Same ^
