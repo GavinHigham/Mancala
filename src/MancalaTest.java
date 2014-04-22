87cbcefe0ffa92531b1be5b0796793a92f75f93d
ginasalh <ginasalh@users.noreply.github.com>
2014-04-22 13:50:57 -0700

    Create MancalaTest.java

---
+++ src/MancalaTest.java
@@ -0,0 +1,12 @@
+/*
+ * This class contains the main method to get the Mancala game started. 
+ *
+ * @author Dakota
+ */
+public class GameRunner
+{
+    public static void main(String[] args)
+    {
+        ApplicationState start = new ApplicationState();
+    }
+}
