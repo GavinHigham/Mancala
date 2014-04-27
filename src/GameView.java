7541817bcc35c7680d5c75c5aa28926d9d078835
Dakota Polenz <polenzdakota@yahoo.com>
2014-04-26 19:29:27 -0700

    Fixed JPanel repainting issues updated Game View
    Made it so the menu page actually displays instead of only showing a
    blank JFrame. It was one line of code that took me a god damn hour to
    figure out and fix. Also updated GameView class to display an undo
    button. Tried to get it to work with a layout manager but failed. Unless
    someone else can figure it out we may have to manually enter in the
    coordinates for the game. Which would be fine since we would have to
    anyway to add the ellipses that contain the playing stones.

--- src/GameView.java
+++ src/GameView.java
@@ -1,24 +1,48 @@
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 /**
- * This is an interface which will be implemented by the other view classes. 
- * 
+ * This is an interface which will be implemented by the other view classes.
+ *
  * @author P.U.G.S.
  */
-abstract class GameView extends JPanel implements ChangeListener
-{
-	int[] board; //For display purposes. Local copy of boards state, may be stale if not updated.
-	boolean player1Turn; //Same ^
-	ApplicationState model;
-	
+abstract class GameView extends JPanel implements ChangeListener {
+
+    int[] board; //For display purposes. Local copy of boards state, may be stale if not updated.
+    boolean player1Turn; //Same ^
+    ApplicationState model;
+
+    public GameView() {
+
+        setLayout(null); //Tried to use other layouts but could not get it to work
+        JPanel playingPane = new JPanel(); //Pane where we can draw the board
+        
+        JButton undoButton = new JButton("Undo");
+        undoButton.setBounds(450, 10, 75, 50); //Size and position of button. Won't resize if expanded
+
+        undoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO add undo functionality
+            }
+        });
+
+        this.add(undoButton);
+
+    }
+
     public abstract void redraw();
-    
+
     public void stateChanged(ChangeEvent e) {
-    	ApplicationState model = (ApplicationState)e.getSource();
-    	board = model.getBoardState();
-    	player1Turn = model.getPlayer1Turn();
-    	redraw();
+        ApplicationState model = (ApplicationState) e.getSource();
+        board = model.getBoardState();
+        player1Turn = model.getPlayer1Turn();
+        redraw();
     }
 }
--- src/MancalaTest.java
+++ src/MancalaTest.java
@@ -8,7 +8,12 @@ public class MancalaTest
     public static void main(String[] args)
     {
         ApplicationState model = new ApplicationState();
+        MenuView menu = new MenuView(model);
         JewelGameView newView = new JewelGameView(model);
+        NumberGameView num = new NumberGameView(model);
+        model.addChangeListener(menu);
         model.addChangeListener(newView);
+        model.addChangeListener(num);
+        model.setState(0);
     }
 }
